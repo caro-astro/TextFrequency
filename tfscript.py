c42308dd1a22141caee97f08628eee3d69d33a70
caro-astro <carolinecaldwell@gmail.com>
2016-11-12 18:10:44 +0000

    Update tfscript.py

--- tfscript.py
+++ tfscript.py
@@ -1,11 +1,12 @@
 from collections import Counter
+from nltk.corpus import stopwords
+import string
+
 f=open("/Users/Caroline/Analysis/LML/goodthings.txt", "r+")
 wordcount = Counter(f.read().split())
 #for k,v in wordcount.items():
 #     print k, v
 print(wordcount.most_common(5))
-from nltk.corpus import stopwords
-import string
 punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + ['\xc2\xb7', 'I', 'love', 'The', 'live', 'get', 'see', 'beautiful', 'little', 'its', 'enjoy', 'street', 'also',"it's", "I'm","Also,", 'It', 'always']
 
