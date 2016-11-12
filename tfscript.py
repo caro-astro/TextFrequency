b17a54bf083ff9b8018e3eb0c0c9be2352899dd6
caro-astro <carolinecaldwell@gmail.com>
2016-11-12 18:09:41 +0000

    Create tfscript.py
    This begins to classify the things we love using word counts. Need to try other advanced grouping and clustering on it.

---
+++ tfscript.py
@@ -0,0 +1,14 @@
+from collections import Counter
+f=open("/Users/Caroline/Analysis/LML/goodthings.txt", "r+")
+wordcount = Counter(f.read().split())
+#for k,v in wordcount.items():
+#     print k, v
+print(wordcount.most_common(5))
+from nltk.corpus import stopwords
+import string
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['\xc2\xb7', 'I', 'love', 'The', 'live', 'get', 'see', 'beautiful', 'little', 'its', 'enjoy', 'street', 'also',"it's", "I'm","Also,", 'It', 'always']
+
+terms_stop = [term for term in fl if term not in stop]
+wordcount = Counter(terms_stop)
+print(wordcount.most_common(5))
